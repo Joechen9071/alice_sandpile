999a85808a790d2eadc279ef84ec87532f63465c
Joe Chen <nmb9071@gmail.com>
2022-09-10 23:07:01 -0400

    Delete controller.py

--- controller.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-
-for i in range(0,10):
-    subprocess.run(["C:/Users/nmb90/AppData/Local/Programs/Python/Python39/python.exe","e:/Non-experiment_data/Sandpile/sandpile_redblack.py"])
